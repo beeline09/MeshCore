02aa64ea68311b2a0285d5942899891221b50625
rogovogor <rogovogor@gmail.com>
2026-05-09 00:04:54 +0300

    build: switch companion CI to *_companion_radio_uni / *_companion_uni envs

--- build.sh
+++ build.sh
@@ -220,10 +220,14 @@ build_companion_firmwares() {
 #  build_firmware "t1000e_companion_radio_ble"
 
   # build all companion firmwares
-  build_all_firmwares_by_suffix "_companion_radio_usb"
-  build_all_firmwares_by_suffix "_companion_radio_ble"
-  build_all_firmwares_by_suffix "_companion_usb"
-  build_all_firmwares_by_suffix "_companion_ble"
+  # uni variants replace ble/usb/wifi for platforms that support WITH_WIFI_SWITCHING
+  build_all_firmwares_by_suffix "_companion_radio_uni"
+  build_all_firmwares_by_suffix "_companion_uni"
+  # legacy ble/usb for platforms without uni
+#  build_all_firmwares_by_suffix "_companion_radio_usb"
+#  build_all_firmwares_by_suffix "_companion_radio_ble"
+#  build_all_firmwares_by_suffix "_companion_usb"
+#  build_all_firmwares_by_suffix "_companion_ble"
 
 }
 
